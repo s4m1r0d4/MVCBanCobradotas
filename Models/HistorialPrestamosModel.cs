e2cea36070635f141346ad0bf469c8ec50265942
Juno <junomidnight8@gmail.com>
2023-06-17 21:35:57 -0600

    HistorialPrestamos

---
+++ Models/HistorialPrestamosModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BanCobradotas.Models;
+
+public class HistorialPrestamosModel
+{
+    public List<Prestamo>? MostrarPrestamo;
+
+}
