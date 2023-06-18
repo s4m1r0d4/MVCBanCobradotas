56866d0f442bc4ea46d109dde9955ce9c23f19b9
s4m1r0d4 <reddragon987pro@gmail.com>
2023-06-18 01:10:19 -0600

    Added SeedData project

---
+++ SeedData/Program.cs
@@ -0,0 +1,16 @@
+﻿using BanCobradotas.Models;
+using static System.Console;
+
+using BanCobradotasContext db = new();
+
+var usuarios = db.Usuarios.OrderBy(u => u.FechaNacimiento);
+
+string line = new('-', 76);
+WriteLine(line);
+WriteLine($"|{"ID Usuario",-12}|{"Nombres",-30}|{"Apellidos",-30}|");
+WriteLine(line);
+foreach (var user in usuarios) {
+    WriteLine($"|{user.IDUsuario,-12}|{user.Nombre,-30}|{user.Apellido,-30}|");
+}
+WriteLine(line);
+
