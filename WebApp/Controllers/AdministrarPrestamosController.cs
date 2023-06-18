20e959514969d5d19463cb9ec30e889187d7d220
s4m1r0d4 <reddragon987pro@gmail.com>
2023-06-17 22:26:08 -0600

    Started

--- WebApp/Controllers/AdministrarPrestamosController.cs
+++ WebApp/Controllers/AdministrarPrestamosController.cs
@@ -8,7 +8,6 @@ using Microsoft.EntityFrameworkCore;
 namespace WebApp.Controllers;
 
 [Authorize(Policy = "EmployeeOnly")]
-
 public class AdministrarPrestamosController : Controller
 {
     private readonly BanCobradotasContext db;
@@ -17,5 +16,28 @@ public class AdministrarPrestamosController : Controller
     {
         db = injectedContext;
     }
-    //tasa de interes calculo
+
+    public async Task<IActionResult> Solicitudes()
+    {
+        var solicitudes = await db.Prestamos.Where(p => p.FechaAprobacion == null).ToListAsync();
+        var model = new HistorialPrestamosModel()
+        {
+            Prestamos = solicitudes
+        };
+        return View(model);
+    }
+
+
+    // Get
+    public async Task<IActionResult> Calcular(long IDPrestamo)
+    {
+        // RQNF12: El cálculo de un préstamo no puede ser mayor al 50% del máximo del total que se encuentra en la cuenta.
+        // Only allow Gerente to accept 
+        string role = HttpContext.User.Claims!.Where(c => c.Type == ClaimTypes.Role).Select(c => c.Value).FirstOrDefault()!;
+        if (role == "Empleado") {
+
+        }
+        throw new NotImplementedException();
+    }
+
 }
