f0b973faec4ca7f5cd9a04b608dd0dfa39e38882
s4m1r0d4 <reddragon987pro@gmail.com>
2023-06-18 10:50:21 -0600

    Fixed Calcular View

--- Models/Prestamo.cs
+++ Models/Prestamo.cs
@@ -7,26 +7,37 @@ namespace BanCobradotas.Models;
 public partial class Prestamo
 {
     [Key]
+    [Display(Name = "ID Presamo")]
     public long IDPrestamo { get; set; }
 
+    [Display(Name = "Fecha de Solicitud")]
     public DateTime FechaSolicitud { get; set; }
 
+    [Display(Name = "Fecha de Aprobacion")]
     public DateTime? FechaAprobacion { get; set; }
 
+    [Display(Name = "Fecha de Liquidación")]
     public DateTime? FechaLiquidacion { get; set; }
 
+    [Display(Name = "Número de meses")]
     public long NumMeses { get; set; }
 
+    [Display(Name = "Pago Mensual")]
     public double? PagoMensual { get; set; }
 
+    [Display(Name = "Interés")]
     public double Interes { get; set; }
 
+    [Display(Name = "Cantidad")]
     public double Cantidad { get; set; }
 
+    [Display(Name = "Estado")]
     public long IDEstado { get; set; }
 
+    [Display(Name = "ID Cuenta Bancaria")]
     public long IDCuentaBancaria { get; set; }
 
+    [Display(Name = "ID Nómina")]
     public long? IDNomina { get; set; }
 
     [InverseProperty("IDPrestamoNavigation")]
