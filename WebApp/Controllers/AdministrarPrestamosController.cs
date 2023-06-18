8da35f560e4c32a800c02ab95e8e1c2af03aeadf
s4m1r0d4 <reddragon987pro@gmail.com>
2023-06-18 02:57:36 -0600

    Refactored Solicitudes to Index

--- WebApp/Controllers/AdministrarPrestamosController.cs
+++ WebApp/Controllers/AdministrarPrestamosController.cs
@@ -17,7 +17,7 @@ public class AdministrarPrestamosController : Controller
         db = injectedContext;
     }
 
-    public async Task<IActionResult> Solicitudes()
+    public async Task<IActionResult> Index()
     {
         var solicitudes = await db.Prestamos.Where(p => p.FechaAprobacion == null).ToListAsync();
         var model = new HistorialPrestamosModel()
@@ -29,15 +29,26 @@ public class AdministrarPrestamosController : Controller
 
 
     // Get
-    public async Task<IActionResult> Calcular(long IDPrestamo)
+    public async Task<IActionResult> Calcular(long? IDPrestamo)
     {
         // RQNF12: El cálculo de un préstamo no puede ser mayor al 50% del máximo del total que se encuentra en la cuenta.
-        // Only allow Gerente to accept 
+        // Tasa de interés
+        // Only allow Gerente to accept prestamos higher than 24 months
+
+        if (IDPrestamo is null)
+            return View();
+
         string role = HttpContext.User.Claims!.Where(c => c.Type == ClaimTypes.Role).Select(c => c.Value).FirstOrDefault()!;
         if (role == "Empleado") {
 
         }
-        throw new NotImplementedException();
+
+        var prestamo = await db.Prestamos.FirstOrDefaultAsync(p => p.IDPrestamo == IDPrestamo);
+
+        if (prestamo is null)
+            return Problem("Prestamo no encontrado");
+
+        return View(prestamo);
     }
 
 }
