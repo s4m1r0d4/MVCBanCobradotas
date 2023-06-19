a13df77fa7ed5eeb481ced81b44b832346e1c5af
s4m1r0d4 <reddragon987pro@gmail.com>
2023-06-19 17:00:31 -0600

    Added queries

--- SeedData/Program.cs
+++ SeedData/Program.cs
@@ -1,41 +1,50 @@
 ﻿using BanCobradotas.Models;
+
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Migrations.Operations;
 using static System.Console;
 
 using BanCobradotasContext db = new();
 
-var usuarios = db.Usuarios.OrderBy(u => u.FechaNacimiento);
+// var usuarios = db.Usuarios.OrderBy(u => u.FechaNacimiento);
 
-string line = new('-', 76);
-WriteLine(line);
-WriteLine($"|{"ID Usuario",-12}|{"Nombres",-30}|{"Apellidos",-30}|");
-WriteLine(line);
-foreach (var user in usuarios) {
-    WriteLine($"|{user.IDUsuario,-12}|{user.Nombre,-30}|{user.Apellido,-30}|");
-}
-WriteLine(line);
+// string line = new('-', 76);
+// WriteLine(line);
+// WriteLine($"|{"ID Usuario",-12}|{"Nombres",-30}|{"Apellidos",-30}|");
+// WriteLine(line);
+// foreach (var user in usuarios) {
+//     WriteLine($"|{user.IDUsuario,-12}|{user.Nombre,-30}|{user.Apellido,-30}|");
+// }
+// WriteLine(line);
 
-var prestamo = await db.Prestamos.Where(p => p.IDPrestamo == 4 && p.FechaAprobacion != null).FirstOrDefaultAsync();
-if (prestamo is null) {
-    WriteLine("Error finding the prestamo");
-    return;
-}
+// var prestamo = await db.Prestamos.Where(p => p.IDPrestamo == 7 && p.FechaAprobacion != null).FirstOrDefaultAsync();
+// if (prestamo is null) {
+//     WriteLine("Error finding the prestamo");
+//     return;
+// }
 
-List<Pago> pagos = new();
-for (int i = 0; i < 6; i++) {
-    Pago p = new()
-    {
-        Fecha = DateTime.Now,
-        Cantidad = (double)prestamo.PagoMensual,
-        Numero = i + 1,
-        IDPrestamo = prestamo.IDPrestamo
-    };
+// List<Pago> pagos = new();
+// for (int i = 0; i < 6; i++) {
+//     Pago p = new()
+//     {
+//         Fecha = DateTime.Now,
+//         Cantidad = (double)prestamo.PagoMensual,
+//         Numero = i + 1,
+//         IDPrestamo = prestamo.IDPrestamo
+//     };
 
-    pagos.Add(p);
-}
+//     pagos.Add(p);
+// }
 
-await db.Pagos.AddRangeAsync(pagos);
-int affected = await db.SaveChangesAsync();
-if (affected != 6)
-    WriteLine("Error añadiendo pagos");
+// await db.Pagos.AddRangeAsync(pagos);
+// int affected = await db.SaveChangesAsync();
+// if (affected != 6)
+//     WriteLine("Error añadiendo pagos");
+
+// When
+var ingresos = await db.Prestamos
+                    .Where(p => (p.FechaAprobacion != null)
+                                && (p.FechaLiquidacion == null))
+                    .SumAsync(p => p.PagoMensual);
+// Then
+WriteLine($"Ingresos : {ingresos}");
